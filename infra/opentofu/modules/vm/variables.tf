fe2413dcb76fe2479cf6f0cb5c501ba7828b741a
arthur <nguekeu96@gmail.com>
2026-06-15 14:18:31 +0200

    feat(infra): OpenTofu configuré pour Infomaniak OpenStack

--- infra/opentofu/modules/vm/variables.tf
+++ infra/opentofu/modules/vm/variables.tf
@@ -1,9 +1,37 @@
-variable "name"          { type = string }
-variable "image_name"    { type = string; default = "Ubuntu 22.04 LTS" }
-variable "flavor_name"   { type = string; default = "a1-ram2-disk20-perf1" }
-variable "key_pair_name" { type = string }
-variable "network_name"  { type = string }
-variable "course"        { type = string }
-variable "owner_email"   { type = string }
-variable "ends_at"       { type = string }
-variable "request_id"    { type = string }
+variable "name" {
+  type = string
+}
+
+variable "image_name" {
+  type    = string
+  default = "Ubuntu 22.04 LTS Jammy Jellyfish"
+}
+
+variable "flavor_name" {
+  type    = string
+  default = "a1-ram2-disk20-perf1"
+}
+
+variable "key_pair_name" {
+  type = string
+}
+
+variable "network_name" {
+  type = string
+}
+
+variable "course" {
+  type = string
+}
+
+variable "owner_email" {
+  type = string
+}
+
+variable "ends_at" {
+  type = string
+}
+
+variable "request_id" {
+  type = string
+}
